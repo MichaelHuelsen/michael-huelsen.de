260fed480f5dd802835c888c3975ee5c40a0b336
micha <micha@MBP>
2018-01-28 21:48:53 +0100

    lots of stuff from the hero tour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+//  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   title = 'michael-huelsen.de';
-  heroes = HEROES;
 }
-
-export class Hero {
-  id: number;
-  name: string;
-};
-
-const HEROES: Hero[] = [
-  { id: 10, name: 'Kirstin' },
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,34 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
 import { AppComponent } from './app.component';
-
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
-  providers: [],
+  imports: [BrowserModule, FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])],
+  declarations: [AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
