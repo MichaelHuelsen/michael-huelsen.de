339b453ad5c214fea1398d70493fb33d998b6d74
Michael Huelsen <github@michael-huelsen.de>
2021-01-27 22:38:32 +0100

    updated

--- src/app/basic/navigation/navigation.component.ts
+++ src/app/basic/navigation/navigation.component.ts
@@ -15,39 +15,27 @@ export class NavigationComponent implements OnInit {
 
   myBrand = "michael-huelsen.de ";
   separator = "|";
-  subLine = "IT Consulting, Solutions, Services";
+  subLine = "Welcome to my personal website.";
   menuItems = [{
     "label": "Home",
     "icon": "fa-home",
     "link": "Home",
     "tooltip": "Home",
-    "id" : "home"
+    "id": "home"
   },
-  /*
-  {
-    "label": "About me",
-    "icon": "fa-info",
-    "link": "About",
-    "tooltip": "About me",
-  },{
-    "label": "Development",
-    "icon": "fa-info",
-    "link": "Development",
-    "tooltip": "Development and Technology",
-  },*/
   {
     "label": "PGP",
     "icon": "fa-info",
     "link": "PGP",
     "tooltip": "My PGP policy",
-    "id" : "pgp"
+    "id": "pgp"
   },
   {
     "label": "Imprint",
     "icon": "fa-info",
     "link": "Imprint",
     "tooltip": "Imprint",
-    "id" : "imprint"
+    "id": "imprint"
   }
   ];
 
