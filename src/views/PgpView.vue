3fd168447b2a5a96816c0856bec829d2cf9a2920
Michael Huelsen <github@michael-huelsen.de>
2024-02-26 22:26:18 +0100

    updated

---
+++ src/views/PgpView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="imprint">
+    <h1>This is a Pgp page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .imprint {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
